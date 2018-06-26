c6a8110c25e0ee7ab6e41977b478fbbcb0cbb525
Pawel Podolak <p.podolak@gmail.com>
2018-06-26 11:18:19 +0200

    string format fix

--- creates/email.js
+++ creates/email.js
@@ -1,38 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
 
-const fs = require('fs');
-const path = require('path');
-
-const eeClient = require('elasticemail-webapiclient').client;
-const sample = require('../samples/sample_email');
+const eeClient = require("elasticemail-webapiclient").client;
+const sample = require("../samples/sample_email");
 
 const sendEmail = (z, bundle) => {
     const options = {
         apiKey: bundle.authData.api_key,
-        apiUri: 'https://api.elasticemail.com/',
-        apiVersion: 'v2'
+        apiUri: "https://api.elasticemail.com/",
+        apiVersion: "v2"
     };
 
     const EE = new eeClient(options);
+    
     let params = {
-        'subject': bundle.inputData.subject,
-        'from': bundle.inputData.from,
-        'isTransactional': false
+        "subject": bundle.inputData.subject,
+        "from": bundle.inputData.from
     };
 
-    if (bundle.inputData.fromName) params.fromName = bundle.inputData.fromName;
-    if (bundle.inputData.replyTo) params.replyTo = bundle.inputData.replyTo;
-    if (bundle.inputData.replyToName) params.replyToName = bundle.inputData.replyToName;
-    if (bundle.inputData.msgCC) params.msgCC =  bundle.inputData.msgCC;
-    if (bundle.inputData.msgBcc) params.msgBcc = bundle.inputData.msgBcc; 
+    if (bundle.inputData.fromName) { 
+        params.fromName = bundle.inputData.fromName;
+    }
+    
+    if (bundle.inputData.replyTo) {
+        params.replyTo = bundle.inputData.replyTo;
+    }
+
+    if (bundle.inputData.replyToName) {
+        params.replyToName = bundle.inputData.replyToName;
+    }
+
+    if (bundle.inputData.msgCC) {
+        params.msgCC = (_.isArray(bundle.inputData.msgCC)) ? _.join(bundle.inputData.msgCC, ", ") : bundle.inputData.msgCC;
+    }
+
+    if (bundle.inputData.msgBcc) {
+        params.msgBcc = (_.isArray(bundle.inputData.msgBcc)) ? _.join(bundle.inputData.msgBcc, ", ") : bundle.inputData.msgBcc; 
+    }
 
     if (bundle.inputData.msgCC || bundle.inputData.msgBcc) {
-        params.msgTo = bundle.inputData.to;
+        params.msgTo = (_.isArray(bundle.inputData.to)) ? _.join(bundle.inputData.to, ", ") : bundle.inputData.to;
     } else {
-        params.to = bundle.inputData.to;
+        params.to = (_.isArray(bundle.inputData.to)) ? _.join(bundle.inputData.to, ", ") : bundle.inputData.to;
     }
 
     if(bundle.inputData.bodyType == "HTML") {
-        params.bodyHtml = bundle.inputData.body
+        params.bodyHtml = bundle.inputData.body;
     } else {
         params.bodyText = bundle.inputData.body;
     }
@@ -48,69 +62,65 @@ const sendEmail = (z, bundle) => {
 }
 
 module.exports = {
-    key: 'email',
-    noun: 'Email',
+    key: "email",
+    noun: "Email",
   
     display: {
-      label: 'Send an Email',
-      description: 'Submit emails. The default, maximum (accepted by us) size of an email is 10 MB in total, with or without attachments included.',
+      label: "Send an Email",
+      description: "Submit emails. The default, maximum (accepted by us) size of an email is 10 MB in total, with or without attachments included.",
       important: true
     },
   
     operation: {
       inputFields: [
         {
-            key: 'subject',
-            label:'Subject',
+            key: "subject",
+            label:"Subject",
             required: true,
         },
         {
-            key: 'from',
-            label: 'From',
+            key: "from",
+            label: "From",
             required: true,
-            helpText: 'From email address. Please note this email should be verfied in Elastic Email system. You can verify it on [settings](https://elasticemail.com/account/#/settings/domains]) page.'
+            helpText: "From email address. Please note this email should be verfied in Elastic Email system. You can verify it on [settings](https://elasticemail.com/account/#/settings/domains]) page."
         },
         {
-            key: 'fromName',
-            label: 'From name',
+            key: "fromName",
+            label: "From name",
             required: false,
-            helpText: 'Display name for from email address'
+            helpText: "Display name for from email address"
         },
         {
-            key: 'replyTo',
-            label: 'Reply to',
+            key: "replyTo",
+            label: "Reply to",
             required: false,
-            helpText: 'Email address to reply to'
+            helpText: "Email address to reply to"
         },
         {
-            key: 'replyToName',
-            label: 'Reply to name',
+            key: "replyToName",
+            label: "Reply to name",
             required: false,
-            helpText: 'Display name of the reply to address'
+            helpText: "Display name of the reply to address"
         },
         {
-            type: "string",
-            list: true,
-            key: 'to',
-            label: 'To',
+            key: "to",
+            label: "To",
             required: true,
-            helpText: 'List of email recipients.'
+            helpText: "List of email recipients."
         },
         {
-            type: "string",
             list: true,
-            key: 'msgCC',
-            label: 'CC',
+            key: "msgCC",
+            label: "CC",
             required: false,
-            helpText: 'List of email recipients (visible to all other recipients of the message as CC MIME header).'
+            helpText: "List of email recipients (visible to all other recipients of the message as CC MIME header)."
         },
         {
-            type: "string",
             list: true,
-            key: 'msgBcc',
-            label: 'Bcc',
+            key: "msgBcc",
+            label: "Bcc",
             required: false,
-            helpText: 'List of hidden email recipients.'
+            helpText: "List of hidden email recipients."
         },
         {
             key: "bodyType",
@@ -125,11 +135,11 @@ module.exports = {
             required: true
         },
         {
-            key: 'attachmentFile',
-            label: 'Attachment',
-            type: 'file',
+            key: "attachmentFile",
+            label: "Attachment",
+            type: "file",
             required: false,
-            helpText: 'Attachment file'
+            helpText: "Attachment file"
         }
       ],
       perform: sendEmail,
--- test/email.js
+++ test/email.js
@@ -1,32 +1,32 @@
-'use strict';
-const should = require('should');
+"use strict";
+const should = require("should");
 
-const zapier = require('zapier-platform-core');
+const zapier = require("zapier-platform-core");
 
-const App = require('../index');
+const App = require("../index");
 const appTester = zapier.createAppTester(App);
 
 const testData = {
-    subject: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    to: 'joandoe@gmail.com',
-    from: 'johndoe@gmail.com',
-    replyTo: 'johndoe@gmail.com',
-    bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-    ' Curabitur in lectus id ipsum laoreet dignissim in ut odio. Duis volutpat arcu dolor, eu cursus mi vestibulum ut.' +
-    ' Etiam eu ipsum orci. Vestibulum aliquam eros in massa dapibus malesuada. Proin sit amet blandit nulla, eu porttitor neque.' +
-    ' Duis consequat erat est, sit amet condimentum turpis sagittis sit amet. Nam fringilla, tellus ac euismod elementum,' +
-    ' ipsum ante consequat nisi, ut lobortis arcu sapien vel ex. Mauris sit amet magna a ipsum porttitor hendrerit.' +
-    ' Nam elementum iaculis tellus, nec euismod ante. Suspendisse nec lobortis magna, at placerat augue.' +
-    ' Quisque luctus scelerisque metus, ut facilisis mi consectetur vel. Ut augue diam, ornare dictum tincidunt a,' +
-    ' volutpat nec arcu. Mauris iaculis bibendum pulvinar. Quisque vestibulum, magna quis aliquam tincidunt,' +
-    ' leo eros luctus nibh, eu dictum nisl velit id mauris.',
-    fromName: 'John Doe',
-    bodyType: 'Plain'
+    subject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    to: "joandoe@gmail.com",
+    from: "johndoe@gmail.com",
+    replyTo: "johndoe@gmail.com",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+    " Curabitur in lectus id ipsum laoreet dignissim in ut odio. Duis volutpat arcu dolor, eu cursus mi vestibulum ut." +
+    " Etiam eu ipsum orci. Vestibulum aliquam eros in massa dapibus malesuada. Proin sit amet blandit nulla, eu porttitor neque." +
+    " Duis consequat erat est, sit amet condimentum turpis sagittis sit amet. Nam fringilla, tellus ac euismod elementum," +
+    " ipsum ante consequat nisi, ut lobortis arcu sapien vel ex. Mauris sit amet magna a ipsum porttitor hendrerit." +
+    " Nam elementum iaculis tellus, nec euismod ante. Suspendisse nec lobortis magna, at placerat augue." +
+    " Quisque luctus scelerisque metus, ut facilisis mi consectetur vel. Ut augue diam, ornare dictum tincidunt a," +
+    " volutpat nec arcu. Mauris iaculis bibendum pulvinar. Quisque vestibulum, magna quis aliquam tincidunt," +
+    " leo eros luctus nibh, eu dictum nisl velit id mauris.",
+    fromName: "John Doe",
+    bodyType: "Plain"
 
 };
 
-describe('email action', () => {
-    it('should send an email', (done) => {
+describe("email action", () => {
+    it("should send an email", (done) => {
         const bundle = {
             authData: {
                 api_key:process.env.API_KEY
@@ -36,7 +36,7 @@ describe('email action', () => {
               to: testData.to,
               from: testData.from,
               replyTo: testData.replyTo,
-              body: testData.bodyText,
+              body: testData.body,
               fromName: testData.fromName,
               bodyType: testData.bodyType
             }
